3c18410c907d3baa8df5283baf68b8a4b13e756d
vitaly <vitalicheg28@mail.ru>
2025-04-07 18:11:11 +0200

    change migrate.sh

--- db/migrate.sh
+++ db/migrate.sh
@@ -7,7 +7,7 @@ DB_NAME="users_db"
 
 echo "Applying migrations from db/migrations/..."
 
-for file in db/migrations/*.up.sql; do
+for file in migrations/*.up.sql; do
     echo "Applying migration: $file"
     PGPASSWORD="postgres" psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$file"
 done
