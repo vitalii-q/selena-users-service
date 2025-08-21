2b0ea9f8616dc712b4b720e0fdf445f311c20d84
vitaly <vitalicheg28@mail.ru>
2025-08-21 15:52:03 +0200

    feat: add ssl db connection in cloud

--- internal/database/db.go
+++ internal/database/db.go
@@ -4,11 +4,21 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/jackc/pgx/v5"
 )
 
 func ConnectDB(host, port, user, password, dbname string) (*pgx.Conn, error) {
+	sslmode := os.Getenv("USERS_POSTGRES_DB_SSLMODE")
+	if sslmode == "" {
+		if os.Getenv("PROJECT_SUFFIX") == "prod" {
+			sslmode = "require"
+		} else {
+			sslmode = "disable"
+		}
+	}
+
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", user, password, host, port, dbname)
 
 	conn, err := pgx.Connect(context.Background(), connStr)
